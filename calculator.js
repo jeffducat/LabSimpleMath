9125e8d7af27c4690964a055cb5a0385243ba347
jeff_ducat <jducat81@gmail.com>
2019-05-23 10:47:41 -0400

    updated Simple Math Lab calculations

--- calculator.js
+++ calculator.js
@@ -1,54 +1,31 @@
-function add() {
-    var num1 = document.getElementById("txt1").value;
-    var num2 = document.getElementById("txt2").value;
-    var num3 = Number(num1) + Number(num2);
-    console.log(num3);
-    document.getElementById("demo").innerText = num3;
-  }
+function onClick() {
+    var input1Field = document.getElementById('input1');
+    var input2Field = document.getElementById('input2');
+    var calculateFields = document.getElementsByName("calculate");
+    var result = document.getElementById('result');
+ 
+    for (var i = 0; i < calculateFields.length; i++) {
+        if (calculateFields[i].checked) {
+            var input1 = Number(input1Field.value);
+            var input2 = Number(input2Field.value);
 
-  function subtract() {
-    var num1 = document.getElementById("txt1").value;
-    var num2 = document.getElementById("txt2").value;
-    var num3 = number(num1) - number(num2);
-    document.getElementById("demo").innerHTML = num3;
-  }
+            switch (i) {
+                case 0:
+                result.innerHTML = input1 + input2;
+                break;
+                case 1:
+                result.innerHTML = input1 - input2;
+                break;
+                case 2:
+                result.innerHTML = input1 * input2;
+                break;
+                case 3:
+                result.innerHTML = input1 / input2;
+                break;
+            }
+            break;
+        }
+    }
+}
 
-  function multiply() {
-    var num1 = document.getElementById("txt1").value;
-    var num2 = document.getElementById("txt2").value;
-    var num3 = number(num1) * number(num2);;
-    document.getElementById("demo").innerHTML = num3;
-  }
-
-  function divide() {
-    var num1 = document.getElementById("txt1").value;
-    var num2 = document.getElementById("txt2").value;
-    var num3 = number(num1) / number(num2);;
-    document.getElementById("demo").innerHTML = num3;
-  }
-
-  function mainCalc() {
- switch(myOperator)
- {
-
-case "add" :
-add();
-break;
-
-case "subtract" :
-subtract();
-break;
-
-case "multiply" :
-multiply();
-break;
-
-case "divide" :
-divide();
-break;
-
-default :
-break;
- }
-} 
 
